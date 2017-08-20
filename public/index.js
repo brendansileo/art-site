39176111b1144d758914500fddb6ff70fb708d2e
Brendan Sileo <brendansileo@gmail.com>
2017-08-20 13:31:27 -0400

    updated image moving, need to fix bounds

--- public/index.js
+++ public/index.js
@@ -18,7 +18,7 @@
 					$scope.im[i] = $scope.im[i].replace("\"", "")
 					$scope.im[i] = $scope.im[i].replace("\"", "")
 				}
-				$scope.currImage = $scope.im.length-3;
+				$scope.currImage = $scope.im.length-1;
 			});
 		
  	}
@@ -33,7 +33,7 @@
  			}
  			else
  			{
- 				return null
+ 				return "//:0"
  			}
  		}
  		else if(i == $scope.imOrder[1])
@@ -48,7 +48,7 @@
  			}
  			else
  			{
- 				return null
+ 				return "//:0"
  			}
  		}
  	}
@@ -81,25 +81,47 @@
 
  	$scope.movePanels = function(i)
  	{
+ 		var width = $(window).width()
+ 		$scope.changeIm(i);
  		if(i == $scope.imOrder[0])
  		{
- 			var width = $(window).width()
-	 		//left panel
-	 		d3.select("#panel" + $scope.imOrder[0]).transition().style("left", .5*width + "px").duration(2000)
-	 		//middle panel
-	 		d3.select("#panel" + $scope.imOrder[1]).transition().style("left", 1.05*width + "px").duration(2000)
-	 		//right panel
-	 		d3.select("#panel" + $scope.imOrder[2]).transition().style("left", 1.2*width + "px").duration(1000)
-	 		d3.select("#panel" + $scope.imOrder[2]).style("left", -.30 * width + "px")
-	 		d3.select("#panel" + $scope.imOrder[2]).transition().style("left", -.05*width + "px").duration(2000)
-	 	}
-	 	//TODO: Update imOrder 
+ 			if($scope.currImage != 0)
+ 			{
+		 		//left panel
+		 		d3.select("#panel" + $scope.imOrder[0]).transition().style("left", .48*width + "px").duration(2000)
+		 		//middle panel
+		 		d3.select("#panel" + $scope.imOrder[1]).transition().style("left", 1.05*width + "px").duration(2000)
+		 		//right panel
+		 		d3.select("#panel" + $scope.imOrder[2]).transition().style("left", 1.20*width + "px").duration(1200)
+		 		d3.select("#panel" + $scope.imOrder[2]).transition().delay(800).style("left", -.15*width + "px").duration(0)
+		 		d3.select("#panel" + $scope.imOrder[2]).transition().delay(800).style("left", -.05*width + "px").duration(1000)
+		 	
+			 	var newOrder = [$scope.imOrder[2], $scope.imOrder[0], $scope.imOrder[1]]
+			 	$scope.imOrder = newOrder
+			 }
+		 }
+		 else if(i == $scope.imOrder[2])
+		 {
+		 	if($scope.currImage != $scope.im.length-1)
+		 	{
+			 	//right panel
+			 	d3.select("#panel" + $scope.imOrder[2]).transition().style("left", .48*width + "px").duration(2000)
+			 	//middle panel
+			 	d3.select("#panel" + $scope.imOrder[1]).transition().style("left", -.05*width + "px").duration(2000)
+			 	//left panel
+		 		d3.select("#panel" + $scope.imOrder[0]).transition().style("left", -.2*width + "px").duration(1200)
+		 		d3.select("#panel" + $scope.imOrder[0]).transition().delay(800).style("left", 1.2*width + "px").duration(0)
+		 		d3.select("#panel" + $scope.imOrder[0]).transition().delay(800).style("left", 1.05*width + "px").duration(1000)
+
+		 		var newOrder = [$scope.imOrder[1], $scope.imOrder[2], $scope.imOrder[0]]
+			 	$scope.imOrder = newOrder
+			 }
+		 }
  	}
 
  	var init = function() 
  	{
  		$scope.listFiles();
- 		$scope.movePanels();
  	}
  	init();
  });
